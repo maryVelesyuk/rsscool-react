6cdfdc9c3894e43a20b1ddc60754b4f5d5226732
Maryia Velesiuk <m.velesiuk@andersenlab.com>
2024-07-07 02:00:28 +0300

    added script to deploy to gh pages, changed error message component

--- src/components/ErrorMessage/ErrorMessage.tsx
+++ src/components/ErrorMessage/ErrorMessage.tsx
@@ -4,6 +4,11 @@ import "./ErrorMessage.css";
 
 export default class ErrorMessage extends Component {
   render() {
-    return <img src={img} alt="error" className="error" />;
+    return (
+      <div className="error-wrapper">
+        <p>Something went wrong!</p>
+        <img src={img} alt="error" className="error" />
+      </div>
+    );
   }
 }
