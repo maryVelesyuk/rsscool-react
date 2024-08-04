c2df2ee50800a7a5cbf2d90168e5fab845a71526
Maryia Velesiuk <m.velesiuk@andersenlab.com>
2024-08-04 12:33:29 +0200

    removed vite and all its settings

--- src/utils/loader.ts
+++
@@ -1,6 +0,0 @@
-export async function loader({ params }: { params: unknown }) {
-  const planetInfo = await fetch(
-    `https://swapi.dev/api/planets/?search=${params!.planetName}`
-  );
-  return planetInfo.json();
-}
